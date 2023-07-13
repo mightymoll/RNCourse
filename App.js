75c34c43218680848742c094e67d5cfbc0efd701
Molly Felts Vallin <mnfelts@gmail.com>
2023-07-13 18:31:51 +0200

    goal list
    - set up states to capture user-entered goal text
    
    - handler functions capture input and add to goals list
    
    - view .map  to display list items as individual text elements

--- App.js
+++ App.js
@@ -1,14 +1,29 @@
 import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import { useState } from 'react';
 
 export default function App() {
+  // create state to store user's new goal text
+  const [goalText, setGoalText] = useState('');
+  const [goalsList, setGoalsList] = useState([]);
+
+  // update new goal text to match user input
+  function goalInputHandler(enteredText) {
+    setGoalText(enteredText)
+  }
+
+  // add new input (goalText) to list of goals (goalsList)
+  function addGoalHandler() {
+    setGoalsList((currentGoalsList) => [...currentGoalsList, goalText]);
+  }
+
   return (
     <View style={styles.appContainer}>
       <View style={styles.inputContainer}>
-        <TextInput style={styles.textInput} placeholder='goal' />
-        <Button title='add goal' />
+        <TextInput style={styles.textInput} placeholder='goal' onChangeText={goalInputHandler} />
+        <Button title='add goal' onPress={addGoalHandler} />
       </View>
       <View style={styles.goalsContainer}>
-        <Text>List of goals...</Text>
+        {goalsList.map((goal) => <Text key={goal}>{goal}</Text>)}
       </View>
     </View>
   );
